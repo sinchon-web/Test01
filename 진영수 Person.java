0a73647865c4a65838118378afff62a07c89f8b1
JEAN <cats.riding@gmail.com>
2020-11-18 14:41:28 +0900

    Update 진영수 Person.java

--- 진영수 Person.java
+++ 진영수 Person.java
@@ -6,19 +6,19 @@ public class Person {
 	private String name;
 	private int age;
 	
-//	멤머 name의 get메서드 정의
+//	멤버 name의 get메서드 정의
 	public String getName() {
 		return name;
 	}
-//	멤머 name의 set메서드 정의
+//	멤버 name의 set메서드 정의
 	public void setName(String name) {
 		this.name = name;
 	}
-//	멤머 age의 get메서드 정의
+//	멤버 age의 get메서드 정의
 	public int getAge() {
 		return age;
 	}
-//	멤머 age의 set메서드 정의
+//	멤버 age의 set메서드 정의
 	public void setAge(int age) {
 		this.age = age;
 	}
