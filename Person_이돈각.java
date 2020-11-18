d61e7af165ebf63da921ca17f5eb0be78dc4d478
LeeDonGak <73805109+LeeDonGak@users.noreply.github.com>
2020-11-18 15:18:40 +0900

    Update Person_이돈각.java

--- Person_이돈각.java
+++ Person_이돈각.java
@@ -4,20 +4,23 @@ package day1118;
 public class Person_이돈각 {
 
 	
-	private String name;//호출 할 수 없는 접근자 선언
-	private int age;	//호출 할 수 없는 접근자 선언
+	private String name;//외부의 접근 제한 private 선언
+	private int age;	//외부의 접근 제한 private 선언
 	
 	
-	
+	//get name 메서드
 	public String getName() {//리턴타입은가지고 파라미터를 정의하지않음
 		return name;
 	}
+	//set name 메서드
 	public void setName(String name) { //파라미터를 정의함
 		this.name = name;			//this.name 은 private로 인한 해당클래스의 name 만 접근
 	}
+	//get Age 메서드
 	public int getAge() { 
 		return age;
 	}
+	//set Age 메서드
 	public void setAge(int age) {
 		this.age = age;
 	}
